1ce7b1c333a2236fc71188e90023f1d84b13c281
devingryu <devingryu@korea.ac.kr>
2022-02-10 20:02:58 +0900

    Clean Code, Added test

--- src/test/kotlin/BitArrayTest.kt
+++ src/test/kotlin/BitArrayTest.kt
@@ -7,18 +7,19 @@ class BitArrayTest {
     @Test
     fun testBitArray() {
         val testBitArray = BitArray(26)
-        with(testBitArray){
-            set(0,true)
-            set(2,true)
-            set(3,true)
-            set(8,true)
-            set(13,true)
-            set(24,true)
-            set(25,true)
+        with(testBitArray) {
+            set(0, true)
+            set(2, true)
+            set(3, true)
+            set(8, true)
+            set(13, true)
+            set(24, true)
+            set(25, true)
         }
-        assertEquals("10110000100001000000000011",testBitArray.toString())
+        assertEquals("10110000100001000000000011", testBitArray.toString())
         assertEquals(true, testBitArray[8])
         assertEquals(false, testBitArray[22])
+        assertEquals(26, testBitArray.size)
         assertFailsWith(IndexOutOfBoundsException::class) { testBitArray[26] }
     }
 }
